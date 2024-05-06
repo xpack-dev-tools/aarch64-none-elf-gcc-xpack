e4e56eb461614c0896136faf830069d5d9ddfbf7
Liviu Ionescu <ilg@livius.net>
2024-05-06 11:04:39 +0300

    versioning.sh: move gcc_cross_build_common to gcc-cross.sh

--- scripts/versioning.sh
+++ scripts/versioning.sh
@@ -9,104 +9,6 @@
 
 # -----------------------------------------------------------------------------
 
-function gcc_cross_build_common()
-{
-  # Build the native dependencies.
-
-  libiconv_build "${XBB_LIBICONV_VERSION}"
-
-  ncurses_build "${XBB_NCURSES_VERSION}"
-
-  # new makeinfo needed by binutils 2.41 and up
-  # checking for suffix of object files...   MAKEINFO doc/bfd.info
-  # /Users/ilg/Work/xpack-dev-tools-build/riscv-none-elf-gcc-13.2.0-1/darwin-x64/sources/binutils-2.41/bfd/doc/bfd.texi:245: Node `Sections' requires a sectioning command (e.g., @unnumberedsubsec).
-
-  # Requires libiconf & ncurses.
-  texinfo_build "${XBB_TEXINFO_VERSION}"
-
-  # -------------------------------------------------------------------------
-
-  if [ "${XBB_REQUESTED_HOST_PLATFORM}" == "win32" ]
-  then
-    echo
-    echo "# Building a bootstrap compiler..."
-
-    # The bootstrap compiler (aarch64-none-elf-gcc),
-    # runs on Linux and produces aarch64 static ELFs.
-    # The executables are not distributed, but the libraries are
-    # copied into the Windows distribution, since building them
-    # with the Windows executable is not realistic.
-
-    gcc_cross_build_dependencies
-
-    gcc_cross_build_all "${XBB_APPLICATION_TARGET_TRIPLET}"
-  fi
-
-  # Switch used during development to test bootstrap.
-  if [ "${XBB_APPLICATION_BOOTSTRAP_ONLY:-""}" != "y" ]
-  then
-
-    # -----------------------------------------------------------------------
-    # Build the target dependencies.
-
-    xbb_reset_env
-    xbb_set_target "requested"
-
-    gcc_cross_build_dependencies
-
-    gdb_cross_build_dependencies
-
-    # -----------------------------------------------------------------------
-    # Build the application binaries.
-
-    xbb_set_executables_install_path "${XBB_APPLICATION_INSTALL_FOLDER_PATH}"
-    xbb_set_libraries_install_path "${XBB_DEPENDENCIES_INSTALL_FOLDER_PATH}"
-
-    # -----------------------------------------------------------------------
-
-    if [ "${XBB_REQUESTED_HOST_PLATFORM}" == "win32" ]
-    then
-      binutils_cross_build "${XBB_BINUTILS_VERSION}" "${XBB_APPLICATION_TARGET_TRIPLET}"
-
-      # As usual, for Windows things require more innovative solutions.
-      # In this case the libraries are copied from the bootstrap,
-      # and only the executables are build for Windows.
-      gcc_cross_copy_linux_libs "${XBB_APPLICATION_TARGET_TRIPLET}"
-
-      (
-        # To access the bootstrap compiler (via CC_FOR_TARGET & Co)
-        xbb_activate_installed_bin
-
-        gcc_cross_build_final "${XBB_GCC_VERSION}" "${XBB_APPLICATION_TARGET_TRIPLET}"
-      )
-    else
-      # For macOS & GNU/Linux build the toolchain natively.
-      gcc_cross_build_all "${XBB_APPLICATION_TARGET_TRIPLET}"
-    fi
-
-    gdb_cross_build "${XBB_APPLICATION_TARGET_TRIPLET}" ""
-
-    if [ "${XBB_WITH_GDB_PY3}" == "y" ]
-    then
-      if [ "${XBB_REQUESTED_HOST_PLATFORM}" == "win32" ]
-      then
-        # Shortcut, use the existing python.exe instead of building
-        # if from sources. It also downloads the sources.
-        python3_download_win "${XBB_PYTHON3_VERSION}"
-        python3_copy_win_syslibs
-      else # linux or darwin
-        # Copy libraries from sources and dependencies.
-        python3_copy_syslibs
-      fi
-
-      gdb_cross_build "${XBB_APPLICATION_TARGET_TRIPLET}" "-py3"
-    fi
-
-  fi
-}
-
-# -----------------------------------------------------------------------------
-
 function application_build_versioned_components()
 {
   # This definition also enables building newlib-nano.
@@ -793,8 +695,9 @@ function application_build_versioned_components()
 
   # ---------------------------------------------------------------------------
 
-  # Switch used during development to test bootstrap.
-  if [ "${XBB_APPLICATION_BOOTSTRAP_ONLY:-""}" != "y" ]
+  # Switch used during development to test bootstrap on Windows.
+  if [ "${XBB_APPLICATION_BOOTSTRAP_ONLY:-""}" != "y" ] ||
+     [ "${XBB_REQUESTED_HOST_PLATFORM}" != "win32" ]
   then
 
     gcc_cross_tidy_up
