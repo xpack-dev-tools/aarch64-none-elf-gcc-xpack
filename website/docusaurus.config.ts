baa0618a41a8079656825038ce958f595edafe93
Liviu Ionescu <ilg@livius.net>
2024-09-20 11:59:54 +0300

    website update footer

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -377,7 +377,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} Liviu Ionescu. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Liviu Ionescu. Built with Docusaurus ${getCustomFields().docusaurusVersion} on ${new Date().toDateString()}.`,
     },
     prism: {
       theme: prismThemes.github,
