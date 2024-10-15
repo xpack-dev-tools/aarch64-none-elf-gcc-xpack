1c571dc9634e331e86a6c4e073ffdcc575433779
Liviu Ionescu <ilg@livius.net>
2024-10-15 03:33:31 +0300

    website: updates

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -42,7 +42,7 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout 
-      title={`Welcome to aarch64-none-elf-gcc`}
+      title="Welcome!"
       description="A binary package with the aarch64-none-elf-gcc executables">
       <HomepageHeader />
       <main>
